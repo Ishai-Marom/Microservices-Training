308dc331aca19c9d4dd7d23b422280708e0984a3
Ishai Marom <maromish@gmail.com>
2024-12-14 16:46:39 +0200

    basic web works

---
+++ WebAPIConsumer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http.Json;
+
+namespace WebAPIConsumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var client = new HttpClient();
+            var host = Environment.GetEnvironmentVariable("HOST") ?? "localhost";
+            var port = Environment.GetEnvironmentVariable("") ?? "5186";
+
+            var responseMessage = client.GetAsync($"http://{host}:{port}/WeatherForecast").Result;
+
+            Console.WriteLine(responseMessage.Content.ReadAsStringAsync().Result.ToString());
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
